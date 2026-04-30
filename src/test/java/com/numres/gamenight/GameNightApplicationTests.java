9ca26dd0aae37fcad797ee473397f6cdca109c7f
AmadouCOUL <camadou784@gmail.com>
2026-04-30 14:49:19 +0200

    initial commit

---
+++ src/test/java/com/numres/gamenight/GameNightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numres.gamenight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameNightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
